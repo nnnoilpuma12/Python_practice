60c84de7b5a0cf3f1b314164ec4d91381c519029
NAIKI　WATANABE <57177604+nnnoilpuma12@users.noreply.github.com>
2021-11-18 22:42:19 +0900

    initial commit
    練習

---
+++ test.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+# print("Hello World!")
+# print("こんにちは！")
+# print("Hello World!")
+
+# 演算
+# print(1+1)
+# print(1-1)
+# print(2*2)
+# print(10/2)
+# print(5%3)
+
+# # # 変数
+# unko = 'l_size'
+# unko_length = 0
+# unko_times = 5.5
+
+# # print(unko_times)
+
+# # 条件分岐と関係演算子
+# # if else elif
+# # ==,!=,<,>,<=,>=
+# if unko_length > 6:
+#   print('ooi')
+# elif unko_length == 0:
+#   print('nai!')
+# else:
+#   print('sukunai')
